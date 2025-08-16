79a94ffaf6a1415f22cee19c54ee926841b8549a
Petrichor <vikachris2007@gmail.com>
2025-08-16 19:44:47 +0300

    feat: add Duration in Epic

--- src/main/java/ru/java/java_kanban/model/Epic.java
+++ src/main/java/ru/java/java_kanban/model/Epic.java
@@ -1,10 +1,16 @@
 package ru.java.java_kanban.model;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Epic extends Task {
     private List<Integer> subtaskIds = new ArrayList<>();
+    private Duration duration;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
 
     public Epic(String name, String description) {
         super(name, description, TaskStatus.NEW);
@@ -19,6 +25,38 @@ public class Epic extends Task {
         }
     }
 
+    public void recalcFromSubtasks(List<Subtask> subtasks) {
+        long totalMinutes = 0;
+        LocalDateTime minStart = null;
+        LocalDateTime maxEnd = null;
+
+        if (subtasks != null) {
+            for (Subtask subtask : subtasks) {
+                if (subtask.getDuration() != null) {
+                    totalMinutes += subtask.getDuration().toMinutes();
+                }
+
+                if (subtask.getStartTime() != null) {
+                    if (minStart == null || minStart.isAfter(subtask.getStartTime())) {
+                        minStart = subtask.getStartTime();
+                    }
+                }
+
+                if (subtask.getEndTime() != null) {
+                    if (maxEnd == null || maxEnd.isBefore(subtask.getEndTime())) {
+                        maxEnd = subtask.getEndTime();
+                    }
+                }
+            }
+        }
+
+        this.duration = (totalMinutes > 0)
+                ? Duration.ofMinutes(totalMinutes)
+                : null;
+        this.startTime = minStart;
+        this.endTime = maxEnd;
+    }
+
     @Override
     public TaskType getType() {
         return TaskType.EPIC;
@@ -30,6 +68,31 @@ public class Epic extends Task {
 
     public void setSubtaskIds(List<Integer> ids) {
         this.subtaskIds = ids;
+    }
+
+    @Override
+    public Duration getDuration() {
+        return duration;
+    }
+
+    @Override
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    @Override
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    @Override
+    public LocalDateTime getEndTime() {
+        return endTime;
     }
 
     @Override
